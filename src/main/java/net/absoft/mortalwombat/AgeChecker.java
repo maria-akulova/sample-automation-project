1411d295eaf486b008d1f0d412b341bb514e1ca2
maria.akulova <maria.akulova.dragon@gmail.com>
2022-04-28 17:44:41 +0200

    add 2 tests about age of players

---
+++ src/main/java/net/absoft/mortalwombat/AgeChecker.java
@@ -0,0 +1,14 @@
+package net.absoft.mortalwombat;
+
+public class AgeChecker {
+    private static final int MIN_AGE = 18 ;
+
+    public boolean canPlayGame (int age) {
+        return age >= MIN_AGE;
+    }
+
+    public boolean isYongUser(int age) {
+        return age < MIN_AGE;
+    }
+}
+
---
+++ src/test/java/net/absoft/mortalwombat/AgeCheckerTest.java
@@ -0,0 +1,17 @@
+package net.absoft.mortalwombat;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AgeCheckerTest {
+    @Test
+    public void testAgedUseCanPlay(){
+        AgeChecker ageChecker = new AgeChecker();
+        Assert.assertTrue(ageChecker.canPlayGame(19), "Wrong age" );
+    }
+    @Test
+    public void testThatTooYongUsersCanNotPlay () {
+        AgeChecker ageChecker = new AgeChecker();
+        Assert.assertFalse(ageChecker.isYongUser(19), "User can not play because of too yong age");
+    }
+}
